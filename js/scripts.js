6dbfca3b4d8f040f655f25fb60cb006ecd1df256
Aundra Miller <student@epicodus.com>
2016-01-15 12:00:35 -0800

    make form functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,42 @@
 $(function() {
 
   $('#survey').submit(function(event) {
-    debugger;
+    // debugger;
     var temperature = $('#temperature').val();
     var necessities = $('#necessities').val();
     var clothes = $('#clothes').val();
     var time = $('#time').val();
     var supplies = $('#supplies').val();
 
-    if (tempreature = 'hate'){
+    if (temperature !== 'hate' && time !== 'eat' || temperature !== 'hate' && time !== 'sleep'){
 
-        $('#resultHate').show();
+      if (necessities === 'no' & supplies === 'no'){
+        $('#resultExplore').show();
+      } else {
+        $('#resultSnow').show();
+      }
+
 
     }
 
     else {
-      //   if (necessities = 'no' && supplies = 'no'){
-      //     ('#resultExplore')
-      //   }
-      //
-      //   if else (necessities = 'no' && supplies = 'yes') {
-      //     ('#resultSnow')
-      //   }
+      $('#resultHate').show();
+      $('#resultSnow, #resultExplore').hide();
     };
 
 
 
 
-
+event.preventDefault();
   });
 
-event.preventDefault();
+
 });
+
+//   if (necessities = 'no' && supplies = 'no'){
+//     ('#resultExplore')
+//   }
+//
+//   if else (necessities = 'no' && supplies = 'yes') {
+//     ('#resultSnow')
+//   }
