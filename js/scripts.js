291588a5ba6c4cbd4f00f228e4943e90b895b6e9
Aundra Miller <student@epicodus.com>
2016-01-15 15:28:53 -0800

    Add README.md and adjust scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,31 @@
 $(function() {
 
   $('#survey').submit(function(event) {
-
+ debugger;
     var temperature = $('#temperature').val();
     var necessities = $('#necessities').val();
     var clothes = $('#clothes').val();
     var time = $('#time').val();
     var supplies = $('#supplies').val();
 
+    //People who like cold weather
     if (temperature !== 'hate' && time !== 'eat' || temperature !== 'hate' && time !== 'sleep'){
-
-      if (time === 'hike') {
+      //People who like to hike and discover
+      if (time === 'hike', 'eat', 'sleep' && temperature !== 'love') {
         $('#resultExplore').show();
         $('#resultSnow, #resultHate').hide();
       }
-
+      //People who like to do sports and activities
       else {
-
-        if (necessities === 'no') {
-          $('#RVsupplies').show();
-          $('#resultSnow').show();
-          $('#resultHate, #resultExplore').hide();
-        }
-        else {
-          $('#resultSnow').show();
-          $('#resultHate, #resultExplore').hide();
-        }
-
+        $('#resultSnow').show();
+        $('#resultExplore, #resultHate').hide();
       }
     }
-
+    //People who hate cold weather
     else {
       $('#resultHate').show();
       $('#resultSnow, #resultExplore').hide();
     };
-
-
-
 
 event.preventDefault();
   });
@@ -44,6 +33,23 @@ event.preventDefault();
 
 });
 
+//Attempt 2:
+// if (necessities === 'no') {
+//   $('#resultSnow').show();
+//   $('#resultHate, #resultExplore').hide();
+// }
+// else {
+//   $('#resultSnow').show();
+//   $('#resultHate, #resultExplore').hide();
+// }
+
+
+
+
+
+
+
+// Attempt 1:
 //   if (necessities = 'no' && supplies = 'no'){
 //     ('#resultExplore')
 //   }
