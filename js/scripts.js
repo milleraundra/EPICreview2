9fec63f488f0c63a1cfd774b8e8c60d4f374700b
Aundra Miller <student@epicodus.com>
2016-01-15 11:31:46 -0800

    adjust radio to select and debug

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(function() {
+
+  $('#survey').submit(function(event) {
+    debugger;
+    var temperature = $('#temperature').val();
+    var necessities = $('#necessities').val();
+    var clothes = $('#clothes').val();
+    var time = $('#time').val();
+    var supplies = $('#supplies').val();
+
+    if (tempreature = 'hate'){
+
+        $('#resultHate').show();
+
+    }
+
+    else {
+      //   if (necessities = 'no' && supplies = 'no'){
+      //     ('#resultExplore')
+      //   }
+      //
+      //   if else (necessities = 'no' && supplies = 'yes') {
+      //     ('#resultSnow')
+      //   }
+    };
+
+
+
+
+
+  });
+
+event.preventDefault();
+});
